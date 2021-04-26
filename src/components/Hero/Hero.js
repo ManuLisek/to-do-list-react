dc1d5f9f8391a664b8e7d928b9e0b846af6e8fd7
Michał Lisowiec <miclisowiec@gmail.com>
2021-04-26 16:34:48 +0200

    Add image address to App.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './Hero.scss';
 
-const Hero = () => (
+const Hero = props => (
   <header className={styles.component}>
-    <h2 className={styles.title}>Things to do</h2>
-    <img src='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' className={styles.image}/>
+    <h2 className={styles.title}>{props.titleText}</h2>
+    <img src={props.image} className={styles.image}/>
   </header>
 );
 
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './List.scss';
-import Hero from '../Hero/Hero'
+import Hero from '../Hero/Hero';
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        <Hero/>
+        <Hero titleText={this.props.title} image={this.props.image}/>
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
