f495dd9169d335fe36a5473d93af63f6ffb997ea
Michał Lisowiec <miclisowiec@gmail.com>
2021-04-26 20:56:26 +0200

    Add component Card

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
-import Column from '../Column/Column'
+import Column from '../Column/Column';
+import Creator from '../Creator/Creator';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
@@ -22,6 +23,23 @@ class List extends React.Component {
       static defaultProps = {
         description: settings.defaultListDescription,
       }
+
+      addColumn(title){
+        this.setState(state => (
+          {
+            columns: [
+              ...state.columns,
+              {
+                key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+                title,
+                icon: 'list-alt',
+                cards: []
+              }
+            ]
+          }
+        ));
+      }
+      
   render() {
     return (
       <section className={styles.component}>
@@ -33,6 +51,9 @@ class List extends React.Component {
             {this.state.columns.map(({key, ...columnProps}) => (
             <Column key={key} {...columnProps} />
             ))}
+        </div>
+        <div>
+          <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
         </div>
       </section>
     )
