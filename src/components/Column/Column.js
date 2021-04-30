1abe8e44076d46b68f7e9c82b31a67c1637aef22
Michał Lisowiec <miclisowiec@gmail.com>
2021-04-30 23:23:51 +0200

    Migrate App, List, Column and Card to Redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Column.scss';
 import Card from '../Card/Card';
-import Creator from '../Creator/Creator';
+// import Creator from '../Creator/Creator';
 import {settings} from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 import PropTypes from 'prop-types';
@@ -9,44 +9,26 @@ import PropTypes from 'prop-types';
 
 class Column extends React.Component {
 
-    state = {
-        cards: this.props.cards || []
-      }
-
     static propTypes = {
         title: PropTypes.string,
     }
 
-    addCard(title){
-        this.setState(state => (
-          {
-            cards: [
-              ...state.cards,
-              {
-                key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-                title,
-                icon: 'list-alt',
-              }
-            ]
-          }
-        ));
-      }
-
     render(){
+        const {title, icon, cards} = this.props;
         return(
         <section className={styles.component}>
             <h3 className={styles.title}>
-            {this.props.title}
-            <span className={styles.icon}> <Icon name={this.props.icon}/> </span>
+                {title}
+                <span className={styles.icon}> <Icon name={icon}/> </span>
             </h3>
             <div className={styles.cards}>
-                {this.state.cards.map(({key, ...cardProps}) => (
-                <Card key={key} {...cardProps} />
+                {cards.map(cardData => (
+                    <Card key={cardData.id} {...cardData} />
                 ))}
             </div>
-            <div>
+            {/* <div>
                 <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-            </div>
+            </div> */}
         </section>
         )
     }
